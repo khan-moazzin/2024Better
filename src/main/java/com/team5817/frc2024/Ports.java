becb0f114eb5bfae84a1bf06c7af45db7cccf106
5817 <sangerroboticsprogramming@gmail.com>
2025-01-11 17:34:04 -0800

    Ports

--- src/main/java/com/team5817/frc2024/Ports.java
+++ src/main/java/com/team5817/frc2024/Ports.java
@@ -15,20 +15,20 @@ public class Ports {
 
 	/* DRIVETRAIN CAN DEVICE IDS */
 	public static final CanDeviceId FL_DRIVE = new CanDeviceId(0, "canivore1");
-	public static final CanDeviceId FL_ROTATION = new CanDeviceId(1, "canivore1");
-	public static final CanDeviceId FL_CANCODER = new CanDeviceId(0, "canivore1");
+	public static final CanDeviceId FL_ROTATION = new CanDeviceId(4, "canivore1");
+	public static final CanDeviceId FL_CANCODER = new CanDeviceId(8, "canivore1");
 
-	public static final CanDeviceId FR_DRIVE = new CanDeviceId(2, "canivore1");
-	public static final CanDeviceId FR_ROTATION = new CanDeviceId(3, "canivore1");
-	public static final CanDeviceId FR_CANCODER = new CanDeviceId(1, "canivore1");
+	public static final CanDeviceId FR_DRIVE = new CanDeviceId(1, "canivore1");
+	public static final CanDeviceId FR_ROTATION = new CanDeviceId(5, "canivore1");
+	public static final CanDeviceId FR_CANCODER = new CanDeviceId(9, "canivore1");
 
-	public static final CanDeviceId BL_DRIVE = new CanDeviceId(4, "canivore1");
-	public static final CanDeviceId BL_ROTATION = new CanDeviceId(5, "canivore1");
-	public static final CanDeviceId BL_CANCODER = new CanDeviceId(2, "canivore1");
+	public static final CanDeviceId BL_DRIVE = new CanDeviceId(2, "canivore1");
+	public static final CanDeviceId BL_ROTATION = new CanDeviceId(6, "canivore1");
+	public static final CanDeviceId BL_CANCODER = new CanDeviceId(10, "canivore1");
 
-	public static final CanDeviceId BR_DRIVE = new CanDeviceId(6, "canivore1");
+	public static final CanDeviceId BR_DRIVE = new CanDeviceId(3, "canivore1");
 	public static final CanDeviceId BR_ROTATION = new CanDeviceId(7, "canivore1");
-	public static final CanDeviceId BR_CANCODER = new CanDeviceId(3, "canivore1");
+	public static final CanDeviceId BR_CANCODER = new CanDeviceId(11, "canivore1");
 
 	public static final CanDeviceId INTAKE_PIVOT = new CanDeviceId(8, "canivore1"); //TODO
 	public static final CanDeviceId INTAKE_ROLLER = new CanDeviceId(9, "canivore1"); //TODO
@@ -39,7 +39,7 @@ public class Ports {
 
 	public static final CanDeviceId INTAKE_CANCODER = new CanDeviceId(19, "canivore1");
 
-	public static final int PIGEON = 20;
+	public static final int PIGEON = 23;
 	
 	public static final CanDeviceId LEDS = new CanDeviceId(21, "rio");
 
