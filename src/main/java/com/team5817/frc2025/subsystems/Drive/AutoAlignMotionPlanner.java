3903b5422e7d76192cf4c8e028ef800584b387c2
Hubert Hsu <sangerroboticsprogramming@gmail.com>
2025-04-05 15:56:27 -0700

    Update at 'Sat Apr 05 15:56:27 PDT 2025'

--- src/main/java/com/team5817/frc2025/subsystems/Drive/AutoAlignMotionPlanner.java
+++ src/main/java/com/team5817/frc2025/subsystems/Drive/AutoAlignMotionPlanner.java
@@ -17,8 +17,8 @@ import org.littletonrobotics.junction.Logger;
  */
 public class AutoAlignMotionPlanner {
 
-    private PIDController mXController = new PIDController(4, 0, 0);
-    private PIDController mYController = new PIDController(4, 0, 0);
+    private PIDController mXController = new PIDController(4, 0, 0.1);
+    private PIDController mYController = new PIDController(4, 0, 0.1);
     private SwerveHeadingController mThetaController;
 
     boolean mAutoAlignComplete = false;
